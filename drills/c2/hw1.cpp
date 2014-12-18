ea574a2f10126a17f3d9b03d5dff8bd7ff1c55b6
Taylor Meek <taylor.meek@dev.null>
2014-12-17 21:43:54 -0800

    drill c2

---
+++ drills/c2/hw1.cpp
@@ -0,0 +1,7 @@
+//no #include here
+int main()
+{
+  cout << "Hello, World!\n";
+  keep_window_open();
+  return 0;
+}
---
+++ drills/c2/hw4.cpp
@@ -0,0 +1,7 @@
+#include "../../std_lib_facilities.h"
+integer main()
+{
+  cout << "Hello, World!\n";
+  keep_window_open();
+  return 0;
+}
